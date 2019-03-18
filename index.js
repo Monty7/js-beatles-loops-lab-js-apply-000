0a606f346f3a15319ffb6a2cca1294e7727f3fb6
voluble-bug-0219 <montensia1@outlook.com>
2019-03-18 15:21:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  let results = [];
+  for(let i = 0; i < musicians.length; i++){
+    results.push(`$(musicians[i]) plays $(instruments[i])`);
+  }
+  return results;
+}
